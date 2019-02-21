36c408e89f88eec2f5593bd5591ef5ef166cd74c
Yuri Tarasov <tarasov.yuri@gmail.com>
2019-02-21 07:40:10 +0200

    changes

--- docs/xpath_checks.rb
+++ docs/xpath_checks.rb
@@ -2,3 +2,9 @@
 //input[id='value']
 //div[text()='text']
 //div[contains(text(),'partial text')]
+//input[contains(@type,'partial attribute value')]
+//table[@role='value']/tbody/tr[2]/td[1]/input
+//input[@type='email']/parent::td/parent::tr/parent::tbody/parent::table
+//input[@type='email']/following-sibling::label
+//input[@type='email']/preceding-sibling::label
+//input[@type='email']/parent::td/following-sibling::td[1]/input 
