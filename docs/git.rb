d3511c129d470f0cb2c33a1b6f78bc5a0ac1a746
Yuri Tarasov <ytarasov@quartileone.com>
2019-07-25 16:17:27 +0300

    git updated

--- docs/git.rb
+++ docs/git.rb
@@ -3,3 +3,11 @@ git add .
 git commit -m 'text'
 git remote add origin 'git url'
 git push -u origin master
+
+git pull
+git checkout -b [name_of_your_new_branch]
+git push remote [name_of_your_new_branch]
+
+git branch -a
+
+git remote add [name_of_your_remote] [name_of_your_new_branch]
